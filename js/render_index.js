e5092e31e48bca36d5a25fa60b62ce2584fc8462
Ilir Xhukaj <ilirxhukaj@gmail.com>
2023-05-29 11:56:57 +0200

    ilir

--- js/render_index.js
+++ js/render_index.js
@@ -1,27 +1,33 @@
 function renderLogin() {
     document.getElementById("login-container").innerHTML = /*html*/ `
-        <form class="login-container" >
-          <h1>Log in</h1>
-          <div class="input-container">
-              <div class="input-field">
-                  <input required class="input" type="email" name="email" id="login-email-input" placeholder="Email">
-                  <img src="./assets/img/email-icon.png">
-              </div>
-              
-              <div class="input-field">
-                  <input required class="input togglePassword" type="password" name="password" id="loginPasswordInput" placeholder="Password">
-                  <img class="toogleImage"  src="./assets/img/password-icon.png">
-             </div>
-            </div>
-       
-          <div class="remember-check">
-              <div class="check"><img id="loginCheckbox" src="./assets/img/unchecked.png">Remember me</div>
-              <a  href="#">Forgot my password</a>
+    <form class="login-container" >
+      <h1>Log in</h1>
+
+      <!-- Eingabefelder für E-Mail und Passwort -->
+      <div class="input-container">
+          <div class="input-field">
+              <input required class="input" type="email" name="email" id="login-email-input" placeholder="E-Mail">
+              <img src="./assets/img/email-icon.png">
           </div>
-          <div class="login-buttons">
-              <button class="btn-dark login-btn">Log in</button>
-              <div class="btn-bright guest-login">Guest Log in</div>
-          </div>
-        </form>
-    `;
+          
+          <div class="input-field">
+              <input required class="input togglePassword" type="password" name="password" id="loginPasswordInput" placeholder="Password">
+              <img class="toogleImage"  src="./assets/img/password-icon.png">
+         </div>
+      </div>
+   
+      <!-- Checkbox für "Angemeldet bleiben" und Link zur Passwortwiederherstellung -->
+      <div class="remember-check">
+          <div class="check"><img id="loginCheckbox" src="./assets/img/unchecked.png">Remember me</div>
+          <a  href="#">Forgot my password</a>
+      </div>
+      
+      <!-- Buttons für Einloggen und Gast-Einloggen -->
+      <div class="login-buttons">
+          <button class="btn-dark login-btn">Log in</button>
+          <div class="btn-bright guest-login">Guest Log in</div>
+      </div>
+    </form>
+`;
+
 }
