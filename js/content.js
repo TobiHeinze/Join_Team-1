93c99735a7906edb14595d465a2c083e3ec8f2f5
TobiHeinze <tobias.heinze@mein.gmx>
2023-06-02 15:49:02 +0200

    content.js created

---
+++ js/content.js
@@ -0,0 +1,55 @@
+let contentArray = [
+	{
+		'users': [
+			{
+				'name': [],
+				'email': [],
+				'password': [],
+				'photo': [],
+			}
+		],
+
+		'settings': [     // Das Array ist immer befüllt mit statischem Inhalt, der bei Neuerstellungen mit in die dynamischen Arrays gerendert wird.
+			{
+				'taskStatus': [],
+				'categoryName': [],
+				'categoryBgColor': [],
+				'priority': [],
+				'contactImageBgColor': [],
+			}
+		],
+
+		'contacts': [
+			{
+				'name': [],
+				'nameInitials': [],
+				'email': [],
+				'phoneNumber': [],
+				'contactImageBgColor': [],
+			}
+		],
+
+		'tasks': [
+			{
+				'taskStatus': [],
+				'title': [],
+				'description': [],
+				'priority': [],
+				'dueDate': [],
+				'categoryName': [],
+				'categoryBgColor': [],
+				'assignedTo': [
+					{
+						'name': [],
+						'nameInitials': [],
+						'contactImageBgColor': [],
+					}
+				],
+			}
+		],
+
+	}
+
+]
+
+
