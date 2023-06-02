e3313ec28bb5163de2b0c2f846651608a317bad7
Albert <wissigkeit@outlook.de>
2023-06-02 16:11:31 +0200

    content.js + empty array

--- js/content.js
+++ js/content.js
@@ -53,3 +53,81 @@ let contentArray = [
 ]
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// das selbe array in leer, wenn man die seite man leer sehen möchte kann man das nutzen.
+// let contentArray = [
+// 	{
+// 		'users': [
+// 			{
+// 				'name': [],
+// 				'email': [],
+// 				'password': [],
+// 				'photo': [],
+// 			}
+// 		],
+
+// 		'settings': [     // Das Array ist immer befüllt mit statischem Inhalt, der bei Neuerstellungen mit in die dynamischen Arrays gerendert wird.
+// 			{
+// 				'taskStatus': [],
+// 				'categoryName': [],
+// 				'categoryBgColor': [],
+// 				'priority': [],
+// 				'contactImageBgColor': [],
+// 			}
+// 		],
+
+// 		'contacts': [
+// 			{
+// 				'name': [],
+// 				'nameInitials': [],
+// 				'email': [],
+// 				'phoneNumber': [],
+// 				'contactImageBgColor': [],
+// 			}
+// 		],
+
+// 		'tasks': [
+// 			{
+// 				'taskStatus': [],
+// 				'title': [],
+// 				'description': [],
+// 				'priority': [],
+// 				'dueDate': [],
+// 				'categoryName': [],
+// 				'categoryBgColor': [],
+// 				'assignedTo': [
+// 					{
+// 						'name': [],
+// 						'nameInitials': [],
+// 						'contactImageBgColor': [],
+// 					}
+// 				],
+// 			}
+// 		],
+
+// 	}
+
+// ]
