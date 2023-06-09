c8b329a83af4eeaa9c87e6b3ff0bfd0266ce25fb
TobiHeinze <tobias.heinze@mein.gmx>
2023-06-09 14:58:13 +0200

    board functions complete

--- js/board.js
+++ js/board.js
@@ -8,7 +8,6 @@ function renderBoardContent() {
 
 		for (let j = 0; j < 4; j++) {
 			if (contentArray['tasks']['taskStatus'][i] == j) {
-				if (contentArray['tasks']['taskStatus'][i] == j) {
 					document.getElementById(`taskStatus${j}`).innerHTML += generateBoardHTML(i);
 				}
 				checkForEmptyTaskSection(j);
@@ -19,7 +18,7 @@ function renderBoardContent() {
 			generateSubtaskSection(i);
 		}
 	}
-}
+
 
 
 function generateBoardHTML(index) {
@@ -111,8 +110,6 @@ function moveTo(taskStatus) {
 	contentArray['tasks']['taskStatus'][currentDraggedElement] = taskStatus;
 	renderBoard();
 }
-
-
 
 
 
