146c524d6fd4ad82eb7ae3e64bf2d0d6ecb8a433
Albert <wissigkeit@outlook.de>
2023-06-15 10:45:45 +0200

    smaller code in task.js and some style tasks

--- js/task.js
+++ js/task.js
@@ -24,47 +24,15 @@ async function renderFloatAddTask() {
         document.getElementById('popUpDiv').classList.add('d-flex');
         slideInPopUp();
         currentSubtasks = [];
-    currentSubtaskStatus = [];
+        currentSubtaskStatus = [];
         document.getElementById('popUpDiv').innerHTML = addTaskFloatHTML();
         renderAddTaskCategoryOptions();
-    renderAddTaskAssignedToOptions();
+        renderAddTaskAssignedToOptions();
     } else {
         document.getElementById("popUpDiv").innerHTML = await renderAddTask();
         document.getElementById('addXButtonTask').classList.remove('d-none');
     }
 }
-
-
-
-// alte version zum aufrufen des add task im contacts bereich
-// /**
-//  * This function renders the add task popup when klick in the contact area to give a special contact a task
-//  * 
-//  */
-// function renderFloatAddTask() {
-//     if (window.innerWidth > 800) {
-//         document.getElementById("content").innerHTML = addTaskFloat();
-//         // hier sollte am besten auch renderaddtask gerendert werden und nicht extra der floating style!
-//         // document.getElementById("popUpDiv").innerHTML = renderAddTask();
-//     } else {
-//         resetContent();
-//         document.getElementById("popUpDiv").innerHTML = renderAddTask();
-//     }
-// }
-
-
-// /**
-//  * This function opens the floating add task editor to add new tasks
-//  * 
-//  */
-// function addTaskFloat() {
-//     document.getElementById('popUpDiv').classList.remove('d-none');
-//     document.getElementById('popUpDiv').classList.add('d-flex');
-//     slideInPopUp();
-//     // hier könnten wir vllt noch den selben addtask einbauen wir  normal nur den mit media query 
-//     // verändern dann müsste man ids nicht doppelt vergeben
-//     document.getElementById('popUpDiv').innerHTML = renderAddTask();
-// }
 
 
 /**
@@ -108,7 +76,7 @@ function renderAddTaskCategoryOptions() {
     }
 }
 
-// die farbpunkte auswählbar machen mit onclick oder so
+
 function addNewCategory() {
     document.getElementById('selectBox1').classList.add('d-none');
     document.getElementById('checkboxes1').classList.add('d-none');
@@ -120,9 +88,8 @@ function addNewCategory() {
             <img onclick="addNewCategoryToArray()" src="./assets/img/black-hook.png" alt="hook-img">
           </div>
         `;
-      }
-      
-      if (document.getElementById('renderAddNewCategoryFloat') !== null) {
+    }
+    if (document.getElementById('renderAddNewCategoryFloat') !== null) {
         document.getElementById('renderAddNewCategoryFloat').innerHTML = /*html*/`
           <div class="new-category-float">
             <input id="newCategoryValue" type="text" placeholder="New category name">
@@ -130,14 +97,9 @@ function addNewCategory() {
             <img onclick="addNewCategoryToArray()" src="./assets/img/black-hook.png" alt="hook-img">
           </div>
         `;
-      }
-    // war mal dafür da color in kategorien auszuwählen, aber das passiert nun random
-    // for (let i = 0; i < contentArray['settings']['categoryBgColor'].length; i++) {
-    //     document.getElementById('addColorToNewCategory').innerHTML += /*html*/`
-    //     <div onclick="" class="circle" style="background-color: ${contentArray['settings']['categoryBgColor'][i]}">
-    // `;
-    // }
+    }
 }
+
 
 function closeNewCategory() {
     document.getElementById('renderAddNewCategory').innerHTML = ``;
@@ -146,17 +108,17 @@ function closeNewCategory() {
     document.getElementById('checkboxes1').classList.remove('d-none');
     document.getElementById('addColorToNewCategory').innerHTML = ``;
 }
+
+
 let category = [];
 function addNewCategoryToArray() {
     category = [];
     category = document.getElementById('newCategoryValue').value;
-    // contentArray['settings']['categoryName'].push(category);
-    // category color auch hinzufügen !
     if (document.getElementById('renderAddNewCategory') !== null) {
-    document.getElementById('renderAddNewCategory').innerHTML = ``;
+        document.getElementById('renderAddNewCategory').innerHTML = ``;
     }
     if (document.getElementById('renderAddNewCategoryFloat') !== null) {
-    document.getElementById('renderAddNewCategoryFloat').innerHTML = ``;
+        document.getElementById('renderAddNewCategoryFloat').innerHTML = ``;
     }
     document.getElementById('selectBox1').classList.remove('d-none');
     document.getElementById('checkboxes1').classList.remove('d-none');
@@ -185,6 +147,7 @@ function categoryOption(index) {
     expanded[0] = false;
 }
 
+
 let randomBgColor = [];
 function categoryOption2() {
     randomBgColor = addRandomBackgroundColorToNewCategory();
@@ -194,7 +157,6 @@ function categoryOption2() {
     <div class="flex align-center">
       <div id="newCategoryName">${category}
       </div>
-      <!-- hier wird die color wahrscheinlich random reingegeben über die if mit js befehl -->
       <div id="addNewCategoryColor" class="circle" style="background-color: ${randomBgColor}">
       </div>
     </div>
@@ -212,20 +174,20 @@ function categoryOption2() {
 function renderAddTaskAssignedToOptions() {
     document.getElementById('checkboxes2').innerHTML += /*html*/`
     <div onclick="addNewContact()" class="option flex">
-      <div class="width-100">Invite new Contact</div>
+      <div class="width-100">Invite new Contact
+      </div>
     </div>
   `;
     for (let i = 0; i < contentArray['contacts']['name'].length; i++) {
         document.getElementById('checkboxes2').innerHTML += /*html*/`
-        <div class="option flex">
+      <div class="option flex">
         <input type="checkbox" id="assignedToOptions${i}" value="${contentArray['contacts']['name'][i]}" />
         <div onclick="toggleCheckbox(${i})" class="width-100">
-        ${contentArray['contacts']['name'][i]}
-      </div>
+          ${contentArray['contacts']['name'][i]}
+        </div>
       </div>
       `;
     }
-
 }
 
 
@@ -233,6 +195,7 @@ function toggleCheckbox(index) {
     let checkbox = document.getElementById(`assignedToOptions${index}`);
     checkbox.checked = !checkbox.checked;
 }
+
 
 /**
  * This function gives back a random color from the given colors in the array
@@ -244,68 +207,27 @@ function addRandomBackgroundColorToNewCategory() {
     return contentArray['settings']['categoryBgColor'][random];
 }
 
-// enter in form validation verhindern
 
-// const formAddTask = document.getElementById('myForm');
-// formAddTask.addEventListener('keydown', function (e) {
-//     if (e.key === 'Enter') {
-//         e.preventDefault();
-//     }
-// });
-
-// document.getElementById("myForm").addEventListener("submit", function(event) {
-//     event.preventDefault(); // Verhindert das Standardverhalten des Formulars, also das Absenden per Enter
-//     console.log("Enter-Taste gedrückt");
-//   });
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     var form = document.getElementById("myAddForm");
-//     if (form) {
-//       form.addEventListener("submit", function(event) {
-//         event.preventDefault();
-//         console.log("Enter-Taste gedrückt");
-//       });
-//     } else {
-//       console.log("Das Formular mit der ID 'myAddForm' wurde nicht gefunden.");
-//     }
-//   });
-
-// function handleInputKeyDown(event) {
-//     if (event.key === "Enter") {
-//       event.preventDefault();
-//       // Hier können Sie den gewünschten Code ausführen, der bei Drücken der Eingabetaste im Input-Feld ausgeführt werden soll
-//       // Zum Beispiel:
-//       console.log("Enter-Taste gedrückt im Input-Feld mit ID:", event.target.id);
-//     }
-//   }
-
-//   // Input-Felder auswählen und Event-Listener hinzufügen
-//   var inputFields = document.querySelectorAll("input");
-//   inputFields.forEach(function(input) {
-//     input.addEventListener("keydown", handleInputKeyDown);
-//   });
-
-/**
- * This function saves onclick when everything in the form is required in the renderAddTaskHTML function
- * 
- */
-async function updateTaskArray() {
-    // await getItem(key);
+// ausgelagerte funktionen von der updatetaskarray funktion um die klein zu halten
+function processTitle() {
     let addTitle = document.getElementById('addTitle').value;
     contentArray['tasks']['title'].push(addTitle);
     console.log(addTitle);
+}
 
+function processDescription() {
     let addDescription = document.getElementById('addDescription').value;
     contentArray['tasks']['description'].push(addDescription);
     console.log(addDescription);
+}
 
+function processDueDate() {
     let addDueDate = document.getElementById('addDueDate').value;
     contentArray['tasks']['dueDate'].push(addDueDate);
     console.log(addDueDate);
+}
 
-
-
-
+function processCategory() {
     //  zur add new category wird eine random color hinzugefügt noch!
     if (document.getElementById('categoryOptionShowSelected').textContent.trim() === "Select task category") {
         contentArray['tasks']['categoryName'].push(""); // Leerer String übergeben für keine kategorie
@@ -333,17 +255,14 @@ async function updateTaskArray() {
         contentArray['tasks']['categoryBgColor'].push(backgroundColor);
         console.log('das ist die farbe die mitgegeben wird:', backgroundColor);
     }
+}
 
-
-
-
+function processTaskOption() {
     // task option ( in progress/ done / awainting feedback) eine null ist immer To Do (feld  0 )
     contentArray['tasks']['taskStatus'].push('0');
+}
 
-
-
-
-
+function processAssignedTo() {
     // assigned to selector 
     let selectedNames = [];
     let checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
@@ -351,10 +270,8 @@ async function updateTaskArray() {
         let selectedName = checkbox.value;
         selectedNames.push(selectedName);
     });
-
     let nameInitials = [];
     let contactImageBgColor = [];
-
     if (selectedNames.length > 0) {
         selectedNames.forEach(selectedName => {
             let contactIndex = contentArray['contacts']['name'].indexOf(selectedName);
@@ -364,7 +281,6 @@ async function updateTaskArray() {
             nameInitials.push(initials);
             contactImageBgColor.push(bgColor);
         });
-
         contentArray['tasks']['assignedTo'].push({
             "name": selectedNames,
             "nameInitials": nameInitials,
@@ -380,12 +296,9 @@ async function updateTaskArray() {
     console.log(selectedNames);
     console.log(nameInitials);
     console.log(contactImageBgColor);
+}
 
-
-
-
-
-
+function processSubtasks() {
     // subtask
     if (currentSubtasks.length > 0) {
         contentArray['tasks']['subtasks'].push({
@@ -400,7 +313,9 @@ async function updateTaskArray() {
             "subtaskStatus": [],
         });
     }
+}
 
+function processPriority() {
     // prio
     if (addPriority.length === 0) {
         addPriority = "low";
@@ -410,6 +325,145 @@ async function updateTaskArray() {
         contentArray['tasks']['priority'].push(addPriority);
         console.log("standart mit ausgewählert prio: ", addPriority);
     }
+}
+
+
+
+/**
+ * This function saves onclick when everything in the form is required in the renderAddTaskHTML function
+ * 
+ */
+async function updateTaskArray() {
+    // await getItem(key);
+
+    processTitle();
+    processDescription();
+    processDueDate();
+    processCategory();
+    processTaskOption();
+    processAssignedTo();
+    processSubtasks();
+    processPriority();
+
+    // let addTitle = document.getElementById('addTitle').value;
+    // contentArray['tasks']['title'].push(addTitle);
+    // console.log(addTitle);
+
+    // let addDescription = document.getElementById('addDescription').value;
+    // contentArray['tasks']['description'].push(addDescription);
+    // console.log(addDescription);
+
+    // let addDueDate = document.getElementById('addDueDate').value;
+    // contentArray['tasks']['dueDate'].push(addDueDate);
+    // console.log(addDueDate);
+
+
+
+
+    //  zur add new category wird eine random color hinzugefügt noch!
+    // if (document.getElementById('categoryOptionShowSelected').textContent.trim() === "Select task category") {
+    //     contentArray['tasks']['categoryName'].push(""); // Leerer String übergeben für keine kategorie
+    //     contentArray['tasks']['categoryBgColor'].push(""); //leerer string für keine farbe übergeben
+    // } else {
+    //     if (document.getElementById('categoryOptionShowSelected').innerHTML == "") {
+    //         //add new category to settings and push to category task 
+    //         let addNewCategory = document.getElementById('newCategoryName').innerHTML;
+    //         // let addNewCategory = addNewCategory1.trim();
+    //         contentArray['tasks']['categoryName'].push(addNewCategory.trim());
+    //         contentArray['settings']['categoryName'].push(addNewCategory.trim());
+    //         contentArray['settings']['categoryBgColor'].push(randomBgColor); // hier wird die random background color gespeichert
+    //         console.log('add new category name:', addNewCategory);
+    //     } else {
+    //         //add only task category option from given options
+    //         let addTaskStatus = document.getElementById('categoryOptionShowSelected2').innerHTML;
+    //         contentArray['tasks']['categoryName'].push(addTaskStatus.trim());
+    //         console.log('add given category:', addTaskStatus);
+    //         // hier wird auch die farbe zur jeweiligen kategorie hinzugefügt
+    //     }
+    //     // zieht die farbe der kategorie raus
+    //     let colorId = document.getElementById('addNewCategoryColor');
+    //     let styleColor = window.getComputedStyle(colorId);
+    //     let backgroundColor = styleColor.getPropertyValue('background-color');
+    //     contentArray['tasks']['categoryBgColor'].push(backgroundColor);
+    //     console.log('das ist die farbe die mitgegeben wird:', backgroundColor);
+    // }
+
+
+
+
+    // // task option ( in progress/ done / awainting feedback) eine null ist immer To Do (feld  0 )
+    // contentArray['tasks']['taskStatus'].push('0');
+
+
+
+
+
+    // // assigned to selector 
+    // let selectedNames = [];
+    // let checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    // checkboxes.forEach(checkbox => {
+    //     let selectedName = checkbox.value;
+    //     selectedNames.push(selectedName);
+    // });
+
+    // let nameInitials = [];
+    // let contactImageBgColor = [];
+
+    // if (selectedNames.length > 0) {
+    //     selectedNames.forEach(selectedName => {
+    //         let contactIndex = contentArray['contacts']['name'].indexOf(selectedName);
+    //         let initials = contentArray['contacts']['nameInitials'][contactIndex];
+    //         let bgColor = contentArray['contacts']['contactImageBgColor'][contactIndex];
+
+    //         nameInitials.push(initials);
+    //         contactImageBgColor.push(bgColor);
+    //     });
+
+    //     contentArray['tasks']['assignedTo'].push({
+    //         "name": selectedNames,
+    //         "nameInitials": nameInitials,
+    //         "contactImageBgColor": contactImageBgColor
+    //     });
+    // } else {
+    //     contentArray['tasks']['assignedTo'].push({
+    //         "name": [],
+    //         "nameInitials": [],
+    //         "contactImageBgColor": [],
+    //     });
+    // }
+    // console.log(selectedNames);
+    // console.log(nameInitials);
+    // console.log(contactImageBgColor);
+
+
+
+
+
+
+    // // subtask
+    // if (currentSubtasks.length > 0) {
+    //     contentArray['tasks']['subtasks'].push({
+    //         "subtask": currentSubtasks,
+    //         "subtaskStatus": currentSubtaskStatus, //automatisch auf open bei hinzufügen
+    //     });
+    //     console.log(currentSubtasks);
+    //     console.log(currentSubtaskStatus);
+    // } else {
+    //     contentArray['tasks']['subtasks'].push({
+    //         "subtask": [],
+    //         "subtaskStatus": [],
+    //     });
+    // }
+
+    // // prio
+    // if (addPriority.length === 0) {
+    //     addPriority = "low";
+    //     contentArray['tasks']['priority'].push(addPriority);
+    //     console.log("ohne ausgewählte prio immer low: ", addPriority);
+    // } else {
+    //     contentArray['tasks']['priority'].push(addPriority);
+    //     console.log("standart mit ausgewählert prio: ", addPriority);
+    // }
 
 
     await setItem(key, contentArray);
